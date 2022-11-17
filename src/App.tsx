b41ee397549ab682c82f6d7be2cfd730563d5700
Pedro Sampaio <hunterfp@live.com>
2022-11-17 17:51:45 -0300

    feat(components): added movable tabs
    Add tabs that can be moved (reordered).

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { ChakraProvider } from '@chakra-ui/react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import './App.css';
 import CustomTitlebar from './components/navigation/CustomTitlebar';
+import MovableTabs from './components/navigation/MovableTabs';
 import Theme from './Theme';
 
 const App: React.FC = () => {
   return (
     <ChakraProvider theme={Theme}>
-      <div id="title" style={{ zIndex: 2000, height: 28 }}>
-        <CustomTitlebar />
-      </div>
-      <div id="root"></div>
+      <DndProvider backend={HTML5Backend}>
+        <div id="title" style={{ zIndex: 2000, height: 28 }}>
+          <CustomTitlebar />
+        </div>
+        <div id="root">
+          <MovableTabs />
+        </div>
+      </DndProvider>
     </ChakraProvider>
   );
 };
