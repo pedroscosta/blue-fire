0aa6812f181fc1748927f65b61e41e24379f5aba
Pedro Costa <scosta.pedro.manoel@gmail.com>
2023-04-27 22:00:44 -0300

    refactor(components): make `Select` more flexible

--- src/components/inputs/Select.tsx
+++ src/components/inputs/Select.tsx
@@ -1,45 +1,94 @@
 import { BoxProps } from '@chakra-ui/react';
-import { OptionBase, Select as RCS } from 'chakra-react-select';
-
-interface Option extends OptionBase {
+import {
+  ChakraStylesConfig,
+  ClearIndicatorProps,
+  ContainerProps,
+  ControlProps,
+  DropdownIndicatorProps,
+  GroupBase,
+  GroupHeadingProps,
+  GroupProps,
+  IndicatorsContainerProps,
+  IndicatorSeparatorProps,
+  InputProps,
+  LoadingIndicatorProps,
+  MenuListProps,
+  MenuProps,
+  MultiValueGenericProps,
+  MultiValueProps,
+  MultiValueRemoveProps,
+  NoticeProps,
+  OptionBase,
+  OptionProps,
+  OptionsOrGroups,
+  PlaceholderProps,
+  Select as RCS,
+  SingleValueProps,
+  ValueContainerProps,
+} from 'chakra-react-select';
+export interface Option extends OptionBase {
   label: string;
   value: string;
 }
 
-interface SelectProps {
-  options: string[];
-  onChange?: (index: number, textValue: string) => void;
-  value?: number;
+export type Group<T> = GroupBase<T>;
+
+const isGroup = <T,>(b: T | Group<T>): b is Group<T> => {
+  const opts = (b as Group<T>).options;
+  return opts !== undefined && Array.isArray(opts);
+};
+
+type Components<T> = {
+  Option?: (props: OptionProps<T, false, Group<T>>) => JSX.Element;
+  Group?: (props: GroupProps<T, false, Group<T>>) => JSX.Element;
+  ClearIndicator?: (props: ClearIndicatorProps<T, false, Group<T>>) => JSX.Element;
+  Control?: (props: ControlProps<T, false, Group<T>>) => JSX.Element;
+  DropdownIndicator?: (props: DropdownIndicatorProps<T, false, Group<T>>) => JSX.Element;
+  GroupHeading?: (props: GroupHeadingProps<T, false, Group<T>>) => JSX.Element;
+  IndicatorsContainer?: (props: IndicatorsContainerProps<T, false, Group<T>>) => JSX.Element;
+  IndicatorSeparator?: (props: IndicatorSeparatorProps<T, false, Group<T>>) => JSX.Element;
+  Input?: (props: InputProps<T, false, Group<T>>) => JSX.Element;
+  LoadingIndicator?: (props: LoadingIndicatorProps<T, false, Group<T>>) => JSX.Element;
+  Menu?: (props: MenuProps<T, false, Group<T>>) => JSX.Element;
+  MenuList?: (props: MenuListProps<T, false, Group<T>>) => JSX.Element;
+  LoadingMessage?: (props: NoticeProps<T, false, Group<T>>) => JSX.Element;
+  NoOptionsMessage?: (props: NoticeProps<T, false, Group<T>>) => JSX.Element;
+  MultiValue?: (props: MultiValueProps<T, false, Group<T>>) => JSX.Element;
+  MultiValueContainer?: (props: MultiValueGenericProps<T, false, Group<T>>) => JSX.Element;
+  MultiValueLabel?: (props: MultiValueGenericProps<T, false, Group<T>>) => JSX.Element;
+  MultiValueRemove?: (props: MultiValueRemoveProps<T, false, Group<T>>) => JSX.Element;
+  Placeholder?: (props: PlaceholderProps<T, false, Group<T>>) => JSX.Element;
+  SelectContainer?: (props: ContainerProps<T, false, Group<T>>) => JSX.Element;
+  SingleValue?: (props: SingleValueProps<T, false, Group<T>>) => JSX.Element;
+  ValueContainer?: (props: ValueContainerProps<T, false, Group<T>>) => JSX.Element;
+};
+interface SelectProps<T> {
+  options: OptionsOrGroups<T, Group<T>>;
+  onChange?: (value: T | null) => void;
+  value?: T;
   containerProps?: Partial<BoxProps>;
   placeHolder?: string;
-  defaultValue?: number;
+  defaultValue?: T;
+  components?: Components<T>;
+  chakraStyles?: ChakraStylesConfig<T, false, Group<T>>;
+  additionalProps?: any;
+  variant?: 'outline' | 'filled' | 'flushed' | 'unstyled';
 }
 
-const Select = ({
-  options,
+const Select = <T = Option,>({
   onChange,
-  value,
-  defaultValue,
   containerProps,
-  placeHolder,
-}: SelectProps) => {
-  const formattedOptions: Option[] = options.map((opt, idx) => ({
-    value: idx.toString(),
-    label: opt,
-  }));
-
+  chakraStyles,
+  additionalProps,
+  components,
+  ...rest
+}: SelectProps<T> & Parameters<typeof RCS<T>>[0]) => {
   return (
-    <RCS<Option>
-      name="colors"
-      className="chakra-react-select"
-      classNamePrefix="chakra-react-select"
+    <RCS<T>
       menuPortalTarget={document.body}
-      options={formattedOptions}
-      placeholder={placeHolder}
       selectedOptionStyle="color"
-      defaultValue={defaultValue ? formattedOptions[defaultValue] : undefined}
-      value={value ? formattedOptions[value] : undefined}
-      onChange={(val) => onChange && val && onChange(Number(val.value), val.label)}
+      components={components}
+      onChange={(val) => onChange && onChange(val)}
       chakraStyles={{
         menuList: (provided) => ({ ...provided, overflowX: 'hidden', overflowY: 'auto' }),
         dropdownIndicator: (provided) => ({
@@ -56,7 +105,11 @@ const Select = ({
           ...provided,
           ...containerProps,
         }),
+        ...chakraStyles,
       }}
+      {...rest}
+      // @ts-ignore
+      {...additionalProps}
     />
   );
 };
